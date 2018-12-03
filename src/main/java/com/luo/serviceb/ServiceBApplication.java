781633917f2070e5c73c70cabd65f07b5f6f4e7a
luoxuesen <luoxuesen@ysstech.com>
2018-12-03 15:46:36 +0800

    commit

--- src/main/java/com/luo/serviceb/ServiceBApplication.java
+++ src/main/java/com/luo/serviceb/ServiceBApplication.java
@@ -3,9 +3,13 @@ package com.luo.serviceb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+//注册服务
 @EnableEurekaClient
+//开启Feign
+@EnableFeignClients
 public class ServiceBApplication {
 
     public static void main(String[] args) {
